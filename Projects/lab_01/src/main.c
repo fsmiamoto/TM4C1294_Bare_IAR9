7274178233fc90565afe77b39898d37ce6b207da
Francisco Miamoto <fsmiamoto@gmail.com>
2021-06-20 13:08:29 -0300

    feat(lab01): update wait constant

--- Projects/lab_01/src/main.c
+++ Projects/lab_01/src/main.c
@@ -6,31 +6,36 @@
 #include "driverlib/systick.h"
 #include "inc/hw_memmap.h"
 
-#define LED_D4 GPIO_PIN_0
-#define ONE_SEC 5000000
 
-#define SYS_CLK_CONFIG SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480
+#define LED_D4 GPIO_PIN_0
+#define ONE_SEC 4500000
+
+#define SYS_CLK_CONFIG                                                         \
+  SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480
 #define SYS_CLK_FREQ (120000000UL)
 
-#define waitFor(cycles) for(int i = 0; i < cycles; i++)
+#define waitFor(cycles) for (int i = 0; i < cycles; i++)
 #define writeToLed(value) GPIOPinWrite(GPIO_PORTF_BASE, LED_D4, value)
 
-void setUpGPIO() {
-   SysCtlClockFreqSet(SYS_CLK_CONFIG, SYS_CLK_FREQ);
-   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);         
-   while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF));
-   GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_D4);
-   GPIOPinWrite(GPIO_PORTF_BASE, LED_D4,0);
-   GPIOPadConfigSet(GPIO_PORTF_BASE, LED_D4, GPIO_STRENGTH_12MA, GPIO_PIN_TYPE_STD);
+void setUp(void) {
+  SysCtlClockFreqSet(SYS_CLK_CONFIG, SYS_CLK_FREQ);
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+  while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
+    ;
+  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_D4);
+  GPIOPinWrite(GPIO_PORTF_BASE, LED_D4, 0);
+  GPIOPadConfigSet(GPIO_PORTF_BASE, LED_D4, GPIO_STRENGTH_12MA,
+                   GPIO_PIN_TYPE_STD);
+  printf("Setup completed\n");
 }
 
-int main(void) {
-   setUpGPIO();
-   printf("Starting...\n");
-   for (;;) {
-      writeToLed(0);
-      waitFor(ONE_SEC);
-      writeToLed(1);
-      waitFor(ONE_SEC);
+void main(void) {
+  setUp();
+
+  for (;;) {
+    writeToLed(0);
+    waitFor(ONE_SEC);
+    writeToLed(1);
+    waitFor(ONE_SEC);
   }
 }
